45d718205fa5e831c1689e32de3a71e4c33ddd33
talib yawari <talib.hussain0196@gmail.com>
2023-11-19 19:57:58 +0500

    assignment nov 19

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">Navbar</a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-mdb-toggle="collapse"
+                        data-mdb-target="/navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <i className="fas fa-bars"></i>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/">Features</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/">Pricing</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
---
+++ src/components/TextForm.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+
+export default function TextForm() {
+    const [text, setText] = useState('');
+    const [letters, setLetters] = useState(0);
+    const [words, setWords] = useState(0);
+    const [time, setTime] = useState(0);
+
+    const handleChange = (e) => {
+        const inputText = e.target.value;
+        setText(inputText);
+        setLetters(inputText.length);
+
+        const wordCount = inputText.trim().split(/\s+/).filter(Boolean).length;
+        setWords(wordCount);
+
+        const averageTime = wordCount === 0 ? 0 : wordCount * 0.5;
+        setTime(averageTime);
+    };
+
+    const handleUppercase = () => {
+        const uppercaseText = text.toUpperCase();
+        setText(uppercaseText);
+    };
+
+    const handleLowercase = () => {
+        const lowercaseText = text.toLowerCase();
+        setText(lowercaseText);
+    };
+
+    const handleClearText = () => {
+        setText('');
+        setLetters(0);
+        setWords(0);
+        setTime(0);
+    };
+
+    return (
+        <div className='container my-5'>
+            <div className='row'>
+                <h1 className="text-center mb-4">Text Analyzer</h1>
+            </div>
+            <div className='row'>
+                <textarea className='form-control' rows="8" value={text} onChange={handleChange}></textarea>
+            </div>
+
+            <div className='row mt-3'>
+                <div className='col-md-4'>
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Word Count</h5>
+                            <p className="card-text">{words}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-4'>
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Character Count</h5>
+                            <p className="card-text">{letters}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-4'>
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Average Time</h5>
+                            <p className="card-text">{time} seconds</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='row mt-3 '>
+                <button className='btn btn-primary mx-2 my-2' onClick={handleUppercase}>Uppercase</button>
+                <button className='btn btn-primary mx-2 my-2' onClick={handleLowercase}>Lowercase</button>
+                <button className='btn btn-danger mx-2 my-2' onClick={handleClearText}>Clear Text</button>
+            </div>
+        </div>
+    );
+}
